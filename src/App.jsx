fd4ce0b65a8b623d97fadc69a86e2a5dd1b022b5
Jose Arnedo <arnedo1@msn.com>
2025-08-06 19:35:25 +0200

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
-import Calculator from './components/Claculator'
+import Calculator from './components/Calculator'  // Fixed the typo here
 import Results from './components/Results'
 
 const App = () => {

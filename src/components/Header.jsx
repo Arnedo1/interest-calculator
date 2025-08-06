62510129a60434637671703a98689ac8ffcf9ff1
Jose Arnedo <arnedo1@msn.com>
2025-08-06 20:33:13 +0200

    latest

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1 className='text-white text-center font-semibold md:text-left md:ml-10 text-3xl mt-16 mb-8 px-4 md:px-0'>
+    <h1 className='text-white text-center font-semibold md:text-left md:ml-10 text-3xl md:mt-16 mt-5 mb-8 px-4 md:px-0'>
       Compound Interest Calculator
     </h1>
   )
